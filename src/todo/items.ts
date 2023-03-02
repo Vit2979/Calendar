84696ddd79f3d22e48df95aae3c4a4f5482a3b65
Vit2979 <86978620+Vit2979@users.noreply.github.com>
2023-03-02 18:07:43 +0300

    Add To-Do Calendar

---
+++ src/todo/items.ts
@@ -0,0 +1,20 @@
+import { constantsTodo } from "./constants_todo";
+
+interface stringObjectType {
+  [key: string]: string[] | string | Date | undefined;
+}
+
+export interface schemaType extends stringObjectType {
+  task: string;
+  date1: string;
+  date2?: string;
+  status: string;
+  tags: string[];
+}
+
+export const schema: schemaType = {
+  task: "",
+  date1: `${new Date(new Date().setMonth(new Date().getMonth() + 1))}`,
+  status: constantsTodo.TASK_IN_PROCESS[0],
+  tags: [],
+};
